72189084b86e30d1413d965ad8ff9d5ebada157b
Kurtney Joseph <josephkurt18@gmail.com>
2023-02-20 19:18:21 -0500

    feat: add celebrate validation in separate module

--- utils/validation.js
+++ utils/validation.js
@@ -1,45 +1,41 @@
-const { celebrate, Joi } = require("celebrate");
+const { celebrate, Joi } = require('celebrate');
 
 const loginValidator = celebrate({
-    body: Joi.object().keys({
-        email: Joi.string().required(),
-        password: Joi.string().required(),
-    }),
+  body: Joi.object().keys({
+    email: Joi.string().required(),
+    password: Joi.string().required(),
+  }),
 });
 
-const registrationValidator = celebrate({
-    body: Joi.object().keys({
-        email: Joi.string().required(),
-        password: Joi.string().required(),
-        name: Joi.string().required().min(2).max(30),
-    }),
+const registerValidation = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required(),
+    password: Joi.string().required(),
+    name: Joi.string().required().min(2).max(30),
+  }),
 });
 
 const createArticleValidator = celebrate({
-    body: Joi.object().keys({
-        keyword: Joi.string().required(),
-        title: Joi.string().required(),
-        text: Joi.string().required(),
-        date: Joi.string().required(),
-        source: Joi.string().required(),
-        link: Joi.string().required(),
-        image: Joi.string().required(),
-    }),
+  body: Joi.object().keys({
+    keyword: Joi.string().required(),
+    title: Joi.string().required(),
+    text: Joi.string().required(),
+    date: Joi.string().required(),
+    source: Joi.string().required(),
+    link: Joi.string().required().uri(),
+    image: Joi.string().required().uri(),
+  }),
 });
 
 const deleteArticleValidator = celebrate({
-    params: Joi.object().keys({
-        articleId: Joi.string().hex().length(24),
-    }),
-    body: Joi.object().keys({
-        link: Joi.string().uri(),
-        image: Joi.string().uri(),
-    }),
+  params: Joi.object().keys({
+    articleId: Joi.string().hex().length(24),
+  }),
 });
 
 module.exports = {
-    loginValidator,
-    registrationValidator,
-    createArticleValidator,
-    deleteArticleValidator,
+  loginValidator,
+  registerValidation,
+  createArticleValidator,
+  deleteArticleValidator,
 };
