71e057941167348183c942a3bd5d2495eab26a5d
Kurtney Joseph <josephkurt18@gmail.com>
2023-02-21 12:03:12 -0500

    refactor: imported and refactored celebrate validation

--- routes/articles.js
+++ routes/articles.js
@@ -1,18 +1,17 @@
-const router = require("express").Router();
-
+const router = require('express').Router();
 const {
-    getArticles,
-    createArticle,
-    deleteArticle,
-} = require("../controllers/article");
-const { auth } = require("../middlewares/auth");
+  getArticles,
+  createArticle,
+  deleteArticle,
+} = require('../controllers/article');
+const { auth } = require('../middlewares/auth');
 const {
-    createArticleValidator,
-    deleteArticleValidator,
-} = require("../utils/validation");
+  createArticleValidator,
+  deleteArticleValidator,
+} = require('../utils/validation');
 
-router.get("/", auth, getArticles);
-router.post("/", auth, createArticleValidator, createArticle);
-router.delete("/:articleId", auth, deleteArticleValidator, deleteArticle);
+router.get('/', auth, getArticles);
+router.post('/', auth, createArticleValidator, createArticle);
+router.delete('/:articleId', auth, deleteArticleValidator, deleteArticle);
 
 module.exports = router;
