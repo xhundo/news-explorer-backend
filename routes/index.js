cec4bd1e42bf3edfb5237c486921d4b5316782aa
Kurtney Joseph <josephkurt18@gmail.com>
2023-02-21 11:59:47 -0500

    feat: added all routes to index.js file

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,17 @@
-const router = require("express").Router();
-const { login, createUser } = require("../controllers/user");
-const NotFoundError = require("../errors/not-found-error");
-const articlesRouter = require("./articles");
-const {
-    loginValidator,
-    registrationValidator,
-} = require("../utils/validation");
-const userRouter = require("./user");
+const router = require('express').Router();
+const { login, createUser } = require('../controllers/user');
+const NotFoundError = require('../errors/not-found-error');
+const { loginValidator, registerValidation } = require('../utils/validation');
+const articlesRouter = require('./articles');
 
-router.use("/articles", articlesRouter);
-router.use("/users", userRouter);
-router.post("/signin", loginValidator, login);
-router.post("/signup", registrationValidator, createUser);
+const userRouter = require('./user');
 
+router.use('/articles', articlesRouter);
+router.use('/users', userRouter);
+router.post('/signin', loginValidator, login);
+router.post('/signup', registerValidation, createUser);
 router.use((req, res, next) => {
-    next(new NotFoundError("This route does not exist"));
+  next(new NotFoundError('The requested route does not exist'));
 });
 
 module.exports = router;
