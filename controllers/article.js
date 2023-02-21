ba066429ba16d3b39b6494f0ab92b73683663f33
Kurtney Joseph <josephkurt18@gmail.com>
2023-02-21 12:01:56 -0500

    refactor: modified res and error msgs

--- controllers/article.js
+++ controllers/article.js
@@ -1,9 +1,10 @@
-const mongoose = require('mongoose')
 const ForbiddenError = require('../errors/forbidden-error');
 const NotFoundError = require('../errors/not-found-error');
 const ValidationError = require('../errors/validation-error');
 const Article = require('../models/article');
-const { createSuccess, successReq } = require('../utils/constants');
+const {
+  createSuccess, successReq, forbiddenErrorMsg, validationErrorMsg, notFoundErrorMsg,
+} = require('../utils/constants');
 
 module.exports.createArticle = (req, res, next) => {
   const {
@@ -27,7 +28,7 @@ module.exports.createArticle = (req, res, next) => {
       if (e.name === 'ValidationError') {
         next(
           new ValidationError(
-            'Article validation failed, fields required!',
+            validationErrorMsg,
           ),
         );
       } else {
@@ -37,7 +38,7 @@ module.exports.createArticle = (req, res, next) => {
 };
 
 module.exports.getArticles = (req, res, next) => {
-  Article.find({owner: req.user._id})
+  Article.find({ owner: req.user._id })
     .then((articles) => {
       res.status(successReq).send([{ articles }]);
     })
@@ -50,24 +51,18 @@ module.exports.deleteArticle = (req, res, next) => {
   const id = req.params.articleId;
   Article.findById(id)
     .orFail(() => {
-      throw new NotFoundError('Requested resource not found');
+      throw new NotFoundError(notFoundErrorMsg);
     })
     .then((article) => {
       if (article.owner.equals(req.user._id)) {
         article.remove(() => res.status(successReq).send([{ data: article }]));
       } else {
         throw new ForbiddenError(
-          'Access to the requested resource is forbidden',
+          forbiddenErrorMsg,
         );
       }
     })
     .catch((e) => {
-      if (e.name === 'ValidationError') {
-        next(new ValidationError('Invalid ID'));
-      } else if (e.name === 'DocumentNotFoundError') {
-        next(new NotFoundError('Article not found'));
-      } else {
-        next(e);
-      }
+      next(e);
     });
 };
