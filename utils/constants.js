1e78476203670b4417ac22ec770631a238557079
Kurtney Joseph <josephkurt18@gmail.com>
2023-02-23 13:09:43 -0500

    refactor: add 403 constant and jwt/server address

--- utils/constants.js
+++ utils/constants.js
@@ -2,10 +2,13 @@ const successReq = 200;
 const badReq = 400;
 const serverFailed = 500;
 const notFound = 404;
+const forbiddenError = 403;
 const createSuccess = 201;
 const conflictError = 409;
 const authError = 401;
 const jwtSecret = 'dev-secret';
+const portAddress = 3001;
+const dbUrl = 'mongodb://127.0.0.1:27017/newsexplorer_db';
 const validationErrorMsg = 'Article validation failed, fields required!';
 const notFoundErrorMsg = 'Requested resource not found';
 const serverErrorMsg = 'An error has occured on the server';
@@ -23,11 +26,14 @@ module.exports = {
   conflictError,
   authError,
   jwtSecret,
+  portAddress,
   validationErrorMsg,
   notFoundErrorMsg,
   serverErrorMsg,
   forbiddenErrorMsg,
   userValidationErrorMsg,
   conflictErrorMsg,
+  dbUrl,
+  forbiddenError,
   notFoundUser,
 };
